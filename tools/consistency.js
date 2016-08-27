482e630de4ea80f6d22667be85e721765ba30b01
Panit Wechasil <panit.wechasil@thomsonreuters.com>
2016-08-27 15:57:40 +0100

    consistency check

--- tools/consistency.js
+++ tools/consistency.js
@@ -15,38 +15,33 @@ MongoClient.connect(config.cache.connectionString, (err, db) => {
 		console.log('MongoDB database connection established');
 		console.log('Reading data from SET.or.th');
 
-		//setReader.read().then(function (data) {
-			var data = [{symbol: 'CHOTI'}];
+		setReader.read().then(function (data) {
 			console.log('Received data from SET.or.th');
 			async.eachSeries(data, 
 				(item, callback) => {
 					console.log('Checking consistency of '+item.symbol);
 					let collection = db.collection(item.symbol);
-					collection.find({d: {"$gte": new Date('2016-08-26T00:00:00.000Z')}}).toArray().then((err, data) => {
-						console.log(data);
-						// if (!err) {
-						// 	if (item.open === data.o &&
-						// 		item.high === data.h &&
-						// 		item.low === data.l &&
-						// 		item.close === data.c &&
-						// 		item.volume === data.v) {
-						// 		console.log('OK');
-						// 	} else {
-						// 		console.log('NOT OK');
-						// 		console.log('  db :', data);
-						// 		console.log('  set:', item);
-						// 	}
-						// } else {
-						// 	console.err(err);
-						// }
-						// callback();
+					collection.findOne({d: {"$eq": new Date('2016-08-26T00:00:00.000Z')}}).then((data) => {
+						if (item.open === data.o &&
+							item.high === data.h &&
+							item.low === data.l &&
+							item.close === data.c) {
+							console.log('OK');
+						} else {
+							console.log('NOT OK');
+							console.log('  db :', data);
+							console.log('  set:', item);
+						}
+						callback();
+					}).catch((ex) => {
+						console.err(ex);
+						callback();
 					});
 				},
 				(err) => {
 
 				}
 			);
-		//});
-
+		});
 	}
 });
